a85dc5edd173e1d14edd9ebbd3c698c8e2778156
Ewelina <ewelina_w@o2.pl>
2018-07-14 15:17:16 +0200

    Add responsive web design to the page

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -149,15 +149,20 @@ fillReviewsHTML = (reviews = self.restaurant.reviews) => {
 createReviewHTML = (review) => {
   const li = document.createElement('li');
   const name = document.createElement('p');
+  const div = document.createElement('div');
+  name.className = 'author';
   name.innerHTML = review.name;
-  li.appendChild(name);
+  div.appendChild(name);
 
   const date = document.createElement('p');
   date.innerHTML = review.date;
-  li.appendChild(date);
+  date.className = 'date';
+  div.appendChild(date);
+  li.appendChild(div);
 
   const rating = document.createElement('p');
   rating.innerHTML = `Rating: ${review.rating}`;
+  rating.className = 'value';
   li.appendChild(rating);
 
   const comments = document.createElement('p');
