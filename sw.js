44dd24aefda0de0c469b690237c20aa585bc50c2
Ewelina <ewelina_w@o2.pl>
2018-07-16 22:48:34 +0200

    Add event listeners

--- sw.js
+++ sw.js
@@ -25,34 +25,33 @@ self.addEventListener('install', event => {
   })
 )
 });
-// self.addEventListener('activate', event => {
-//   event.waitUntil(
-//     caches.keys().then(cacheNames => {
-//       return Promise.all(
-//         cacheNames.filter(cacheName => {
-//           return cacheName.startsWith('mws-') &&
-//                  cacheName != staticCacheName;
-//         }).map(cacheName => {
-//           return caches.delete(cacheName);
-//         })
-//       );
-//     })
-//   );
-// });
-//
-// self.addEventListener('fetch', event => {
-//   event.respondWith(
-//     caches.match(event.request).then(response => {
-//       if (response) return response;
-//         return fetch(event.request);
-//     }).catch((error) =>
-//       console.log(error);
-//     )
-//   );
-// });
-//
-// self.addEventListener('message', event => {
-//   if (event.data.action === 'skipWaiting') {
-//     self.skipWaiting();
-//   }
-// });
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.filter(cacheName => {
+          return cacheName.startsWith('mws-') &&
+                 cacheName != staticCacheName;
+        }).map(cacheName => {
+          return caches.delete(cacheName);
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      if (response) return response;
+        return fetch(event.request);
+    }).catch((error) =>
+      console.log(error))
+  );
+});
+
+self.addEventListener('message', event => {
+  if (event.data.action === 'skipWaiting') {
+    self.skipWaiting();
+  }
+});
